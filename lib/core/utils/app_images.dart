421aa77f8f4f2f0c7af3e8463efee5949e41a253
aliNassef <anassef798@gmail.com>
2024-12-12 19:27:45 +0200

    create appImages class

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,3 +1,9 @@
 abstract class AppImages {}
 
-abstract class AppSvgs {}
+abstract class AppSvgs {
+  static const String boarding1 = 'assets/images/onboarding1.svg';
+  static const String boarding2 = 'assets/images/onboarding2.svg';
+  static const String boarding3 = 'assets/images/onboarding3.svg';
+  static const String start = 'assets/images/start.svg';
+  static const String mother = 'assets/images/Mother.svg';
+}
