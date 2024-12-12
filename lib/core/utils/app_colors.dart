060c7a00f78b37c5e66822cc2d0d9a011d383b1f
aliNassef <anassef798@gmail.com>
2024-12-12 19:27:15 +0200

    create appcolors class

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,4 +1,9 @@
- 
+import 'package:flutter/material.dart';
+
 abstract class AppColors {
- 
+  static const primaryColor = Color(0xff4F5C8C);
+  static const secondaryColor = Color(0xffFDB3CA);
+  static const mainPinkColor = Color(0xffFDB3CA);
+  static const darkBlueColor = Color(0xff5C6898);
+  static const greenLightColor = Color(0xffA9DFE2);
 }
