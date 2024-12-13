eb74087728a463237cce9044d15f74d5aac4aded
aliNassef <anassef798@gmail.com>
2024-12-13 21:57:14 +0200

    .

--- lib/features/boarding/presentation/widgets/start_view_body.dart
+++ lib/features/boarding/presentation/widgets/start_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:napd/core/extensions/mediaquery_size.dart';
 import 'package:napd/core/utils/app_colors.dart';
 import 'package:napd/core/utils/app_images.dart';
