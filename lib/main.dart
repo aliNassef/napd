b4712824e2debf43d59313f7513dd96a2c774a2a
aliNassef <anassef798@gmail.com>
2024-12-20 01:03:55 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/cache/cache_helper.dart';
 import 'core/di/service_locator.dart';
 import 'core/widgets/custom_bloc_observer.dart';
@@ -11,6 +12,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupServiceLocator();
   await CacheHelper.init();
+  await ScreenUtil.ensureScreenSize();
   Bloc.observer = CustomBlocObserver();
   bool useDevicePreview = true;
   if (useDevicePreview) {
