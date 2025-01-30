632385190f1e0b398cebc68e8a34ad07d8e6b83b
aliNassef <anassef798@gmail.com>
2025-01-30 10:35:40 +0200

    feat(groups): add ChatBotView to GroupView; implement chat bot interface

---
+++ lib/features/groups/presentation/view/chat_bot_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChatBotViewBody extends StatelessWidget {
+  const ChatBotViewBody({super.key});
+  static const routeName = 'chat-bot';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.cyanAccent,
+    );
+  }
+}
