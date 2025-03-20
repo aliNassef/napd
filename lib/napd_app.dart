3927d45ce5bf4804e0ca0bab15d09b159f7ec3c9
aliNassef <anassef798@gmail.com>
2025-03-20 07:13:23 +0200

    .

--- lib/napd_app.dart
+++ lib/napd_app.dart
@@ -6,8 +6,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/utils/app_router.dart';
 import 'core/utils/app_themes.dart';
 import 'features/shop/presentation/view/cart_view.dart';
-import 'features/splash/presentation/view/splash_view.dart';
-
+ 
 class Napd extends StatelessWidget {
   const Napd({super.key});
 
