f4627e8c740d70583f0dd9ad9513ca2c0bf20a20
aliNassef <anassef798@gmail.com>
2025-04-07 13:02:40 +0200

    feat(firebase): integrate Firebase services and add configuration files

--- lib/core/utils/app_intialization.dart
+++ lib/core/utils/app_intialization.dart
@@ -4,15 +4,19 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../firebase_options.dart';
 import '../cache/cache_helper.dart';
 import '../di/service_locator.dart';
 import '../helpers/notification_service.dart';
 import '../widgets/custom_bloc_observer.dart';
-
-class AppIntialization {
+import 'package:firebase_core/firebase_core.dart';
+ class AppIntialization {
   AppIntialization._();
   static Future<void> initializer() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     await EasyLocalization.ensureInitialized();
     await setupServiceLocator();
     await CacheHelper.init();
